204ebf3c282eb57dba92c100626d83a3154235cf
Lige <lige.williamson@gmail.com>
2019-02-09 03:38:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(int)
+  if int <= 3
+    n > 1
+  elsif int % 2 == 0 || int % 3 == 0
+    false
+  end
+  i = 5
+  while i * i <= int
+    if int % i == 0 || n % (i + 2) == 0
+      false
+    end
+    i += 6
+  end
+  true
+end
