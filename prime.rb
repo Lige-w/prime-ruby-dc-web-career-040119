03402dff2aa4faf222597dabd7c28c963ef7aea5
Lige <lige.williamson@gmail.com>
2019-02-09 03:50:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,14 +2,14 @@
 def prime?(int)
   true if int == 2 || int == 3
   false if int % 2 == 0 || int % 3 == 0
-#   i = 5
-#   w = 2
-#   while i * i <= int
-#     false if int % i == 0
-#     i += w
-#     w = 6 - w
-#   end
-#   true
-# end
+  i = 5
+  w = 2
+  while i * i <= int
+    false if int % i == 0
+    i += w
+    w = 6 - w
+  end
+  true
+end
 
 puts prime?(6)
