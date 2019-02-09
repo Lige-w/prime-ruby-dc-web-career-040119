e64abf9d7078db92f1b85ff83a87eba83cdda5ef
Lige <lige.williamson@gmail.com>
2019-02-09 03:51:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,13 @@
 def prime?(int)
   true if int == 2 || int == 3
   false if int % 2 == 0 || int % 3 == 0
-#   i = 5
-#   w = 2
-#   while i * i <= int
-#     false if int % i == 0
-#     i += w
-#     w = 6 - w
-#   end
+  i = 5
+  w = 2
+  while i * i <= int
+    false if int % i == 0
+    i += w
+    w = 6 - w
+  end
 #   true
 end
 #
