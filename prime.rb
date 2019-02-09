6e143d88ffd5441f240788b100987cc6e6463dd1
Lige <lige.williamson@gmail.com>
2019-02-09 04:01:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(int)
+  return false if int <= 1
+  return true if int == 2 || int == 3
+  return false if int % 2 == 0 || int % 3 == 0
+  i = 5
+  w = 2
+  while i * i <= int
+    return false if int % i == 0
+    i += w
+    w = 6 - w
+  end
+  true
+end
+
+puts prime?(-1)
